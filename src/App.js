c8b3edfe25a4e4a6fb710fa9062ff457f4506e27
User-niomic <user@niomic.co.id>
2021-08-21 20:09:38 +0700

    Tugas 6 React JS

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import MenuHome from "./Page/MenuHome";
+import Kontak from "./Page/Kontak";
+
+function App() {
+  return (
+    <div>
+      <MenuHome/>
+      <br/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Lib/DaftarMinuman.js
@@ -0,0 +1,29 @@
+const DaftarMinuman = [
+    {
+        NamaMakanan: "Es Cendol",
+        Harga : "10.000",
+        img: "https://www.gotravelly.com/blog/wp-content/uploads/2017/12/minuman-tradisional-indonesia-cendol.jpg"
+    },
+    {
+        NamaMakanan: "Es Pisang Hijau",
+        Harga : "10.000",
+        img: "https://www.gotravelly.com/blog/wp-content/uploads/2017/12/minuman-tradisional-indonesia-es-pisang-hijau.jpg"
+    },
+    {
+        NamaMakanan: "Es Kacang Merah",
+        Harga : "10.000",
+        img: "https://www.gotravelly.com/blog/wp-content/uploads/2017/12/minuman-tradisional-indonesia-es-kacang-merah.jpg"
+    },
+    {
+        NamaMakanan: "Es Pleret",
+        Harga : "10.000",
+        img: "https://www.gotravelly.com/blog/wp-content/uploads/2017/12/minuman-tradisional-indonesia-es-pleret.jpg"
+    },
+    {
+        NamaMakanan: "Es Legen",
+        Harga : "15.000",
+        img: "https://www.gotravelly.com/blog/wp-content/uploads/2017/12/minuman-tradisional-indonesia-es-legen.jpg"
+    },
+];
+
+export default DaftarMinuman;
